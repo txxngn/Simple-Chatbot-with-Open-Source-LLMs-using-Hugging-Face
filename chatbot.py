cfde9144b26b33996c10ab158635f3c802570d7c
Thai Nguyen <thainguyen20594@gmail.com>
2024-12-22 14:32:32 -0500

    append to conversation_history

--- chatbot.py
+++ chatbot.py
@@ -25,7 +25,10 @@ input_text ="hello, how are you doing?"
 inputs = tokenizer.encode_plus(history_string, input_text, return_tensors="pt")
 print(inputs)
 
-#and run the file to see tokeniztion
+#Result:
+#{'input_ids': tensor([[1710,   86,   19,  544,  366,  304,  929,   38]]), 
+#'attention_mask': tensor([[1, 1, 1, 1, 1, 1, 1, 1]])}
+
 
 #This attribute provides a mapping of pretrained models
 tokenizer.pretrained_vocab_files_map
@@ -36,10 +39,38 @@ tokenizer.pretrained_vocab_files_map
 outputs = model.generate(**inputs)
 print(outputs)
 #and run the file 
+#->tensor([[   1,  281,  476,  929,  731,   21,  281,  632,  929,  712,  731,   21,
+#          855,  366,  304,   38,  946,  304,  360,  463, 5459, 7930,   38,    2]])
 
 
 #The ouput is a dictionary and contain tokens, now need to be decoded
 #decode the first index of outputs to see the response in plaintext.
 response = tokenizer.decode(outputs[0], skip_special_tokens=True).strip()
 print(response)
-#and run the file 
+
+#->I'm doing well. I am doing very well. How are you? Do you have any hobbies?
+
+
+
+#add both the input and response to conversation_history in plaintext.
+conversation_history.append(input_text)
+conversation_history.append(response)
+print(conversation_history)
+
+#-> ['hello, how are you doing?', "I'm doing well. I am doing very well. How are you? Do you have any hobbies?"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
